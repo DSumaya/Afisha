67a548198165293aca0b98e090f443bca5b2c74e
DSumaya <summa320808@gmail.com>
2025-02-06 18:01:22 +0600

    hw-1

--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -4,14 +4,14 @@ from movie_app.models import Director, Movie, Review
 class DirectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Director
-        fields = '__all__'
+        fields = 'id name'.split()
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = 'id title description duration director'.split()
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = '__all__'
+        fields = 'id text movie'.split()
