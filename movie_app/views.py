2787ba66f0979c0e4e5e1ce8b10037b772623465
DSumaya <summa320808@gmail.com>
2025-02-15 15:16:21 +0600

    hw-4

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,5 +1,3 @@
-from django.core.serializers import serialize
-from django.db.migrations.serializer import Serializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
